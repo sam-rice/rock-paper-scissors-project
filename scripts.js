9a8ec5141c82176ad0ed1866dde89c2895de6247
Sam Rice <samsrice@gmail.com>
2022-09-25 14:06:36 -0700

    Ready directory for merge with main

--- scripts.js
+++ scripts.js
@@ -4,6 +4,11 @@ var computerPlayer;
 
 //----------------------QUERY SELECTORS----------------------//
 
+var mainHTML = document.getElementById("main");
+var header = document.getElementById("header");
+
+var landingMessage = document.getElementById("landing-message");
+
 var namePage = document.getElementById("choose-name");
 var submitNameButton = document.getElementById("submit-name");
 var nameInput = document.getElementById("name-input");
@@ -28,42 +33,52 @@ var userWinsMessage = document.getElementById("user-winner");
 var compWinsMessage = document.getElementById("comp-winner");
 var drawMessage = document.getElementById("draw");
 
+var mainVid = document.getElementById("main-vid");
 
 //----------------------EVENT LISTENERS----------------------//
 
-submitNameButton.addEventListener("click", function () {
-  createHumanPlayer(nameInput.value, emojiInput.value);
-  startNewGame();
-  hide(namePage);
-  show(gameTypePage);
-  show(asideLeft);
-  show(asideRight);
-  updateUserInfo();
-  updatePoints();
+mainVid.addEventListener("click", function () {
+  mainVid.play();
+  fadeOut(landingMessage);
+  setTimeout(hide, 1200, landingMessage);
+  setTimeout(pauseVid, 5000);
+  setTimeout(show, 3500, mainHTML);
+  setTimeout(show, 3500, header);
 });
+
+submitNameButton.addEventListener("click", submitNameFunctions);
 
 basicButton.addEventListener("click", function () {
   currentGame.gameType = "basic";
-  hide(gameTypePage);
-  show(gameplayPage);
-  show(changeGameButton);
+  fadeOut(gameTypePage);
+  setTimeout(hide, 2000, gameTypePage);
+  setTimeout(removeFadeOut, 2000, gameTypePage);
+  setTimeout(show, 2000, gameplayPage);
+  setTimeout(show, 2000, changeGameButton);
   hide(cup);
   hide(matches);
 });
 
 advancedButton.addEventListener("click", function () {
   currentGame.gameType = "advanced";
-  hide(gameTypePage);
-  show(gameplayPage);
-  show(changeGameButton); 
+  fadeOut(gameTypePage);
+  setTimeout(hide, 2000, gameTypePage);
+  setTimeout(removeFadeOut, 2000, gameTypePage);
+  setTimeout(show, 2000, gameplayPage);
+  setTimeout(show, 2000, changeGameButton); 
 });
 
 changeGameButton.addEventListener("click", function () {
   startNewGame();
-  hide(gameplayPage);
-  show(gameTypePage);
-  show(cup);
-  show(matches);
+  fadeOut(gameplayPage);
+  fadeOut(changeGameButton);
+  setTimeout(hide, 2000, changeGameButton);
+  setTimeout(hide, 2000, gameplayPage);
+  setTimeout(removeFadeOut, 2000, gameplayPage);
+  setTimeout(removeFadeOut, 2000, changeGameButton);
+  setTimeout(show, 2000, gameTypePage);
+  setTimeout(show, 2000, cup);
+  setTimeout(show, 2000, matches);
 });
 
 gameplayPage.addEventListener("click", function (event) {
@@ -82,34 +97,12 @@ function createHumanPlayer(name, emoji) {
 };
 
 function startNewGame() {
-  currentGame = new Game (user, computerPlayer)
+  currentGame = new Game (user, computerPlayer);
 };
 
 //----------------------EVENT HANDLERS - DOM ----------------------//
 
-function announceUserWinner() {
-  hide(gameplayPage);
-  show(userWinsMessage);
-  userWinsMessage.innerHTML = `
-    <p>${user.name} wins!</p>
-    `;
-  setTimeout(hide, 2000, userWinsMessage)
-  setTimeout(show, 2000, gameplayPage)
-};
-
-function announceCompWinner() {
-  hide(gameplayPage);
-  show(compWinsMessage);
-  setTimeout(hide, 2000, compWinsMessage)
-  setTimeout(show, 2000, gameplayPage)
-};
-
-function announceDraw() {
-  hide(gameplayPage);
-  show(drawMessage);
-  setTimeout(hide, 2000, drawMessage)
-  setTimeout(show, 2000, gameplayPage)
-};
+//TRANSITIONS
 
 function hide(element) {
   element.classList.add("hidden");
@@ -117,6 +110,78 @@ function hide(element) {
 
 function show(element) {
   element.classList.remove("hidden");
+};
+
+function fadeOut(element) {
+  element.classList.add("fadeout");
+};
+
+function removeFadeOut(element) {
+  element.classList.remove("fadeout");
+}
+
+//OTHER
+
+function submitNameFunctions() {
+  createHumanPlayer(nameInput.value, emojiInput.value);
+  startNewGame();
+  updateUserInfo();
+  updatePoints();
+  fadeOut(namePage);
+  fadeOut(mainHTML);
+  fadeOut(asideLeft);
+  fadeOut(asideRight);
+  fadeOut(header);
+  resumeVid();
+  setTimeout(hide, 2000, namePage);
+  setTimeout(hide, 2000, mainHTML);
+  setTimeout(hide, 2000, asideLeft);
+  setTimeout(hide, 2000, asideRight);
+  setTimeout(hide, 2000, header);
+  setTimeout(removeFadeOut, 2000, mainHTML);
+  setTimeout(removeFadeOut, 2000, asideLeft);
+  setTimeout(removeFadeOut, 2000, asideRight);
+  asideLeft.classList.add("aside-full-speed-fade");
+  asideRight.classList.add("aside-full-speed-fade");
+  setTimeout(removeFadeOut, 2000, header);
+  setTimeout(show, 9000, mainHTML);
+  setTimeout(show, 9000, gameTypePage);
+  setTimeout(show, 9000, asideLeft);
+  setTimeout(show, 9000, asideRight);
+  setTimeout(show, 9000, header);
+  setTimeout(pauseVid, 9000);
+};
+
+function pauseVid() {
+  mainVid.pause();
+};
+
+function resumeVid() {
+  mainVid.play();
+};
+
+function announceUserWinner() {
+  hide(gameplayPage);
+  show(userWinsMessage);
+  userWinsMessage.innerHTML = `
+    <p>${user.name} wins!</p>
+    `;
+  setTimeout(hide, 2000, userWinsMessage);
+  setTimeout(show, 2000, gameplayPage);
+};
+
+function announceCompWinner() {
+  hide(gameplayPage);
+  show(compWinsMessage);
+  setTimeout(hide, 2000, compWinsMessage);
+  setTimeout(show, 2000, gameplayPage);
+};
+
+function announceDraw() {
+  hide(gameplayPage);
+  show(drawMessage);
+  setTimeout(hide, 2000, drawMessage);
+  setTimeout(show, 2000, gameplayPage);
 };
 
 function updateUserInfo() {
@@ -134,3 +199,18 @@ function updatePoints() {
     <p>points: ${computerPlayer.wins}</p>
     `;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
